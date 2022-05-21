52262d61ce56298ee743bd5981b15e38e5e6e382
lyj00912 <3122742760@qq.com>
2022-05-21 08:37:09 +0800

    created C04 - HelloWindow.cpp

---
+++ C04 - HelloWindow.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+#define ScreenFactor (2)
+#define ScreenWidth  (800 * ScreenFactor)
+#define ScreenHeight (600 * ScreenFactor)
+
+int main() {
+
+	// Init GLFW
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create a Window
+	GLFWwindow* window = glfwCreateWindow(
+		ScreenWidth, ScreenHeight, "LearnOpenGL_C04", NULL, NULL
+	);
+	if (window == NULL)
+	{
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Init GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Set the Viewport
+	glViewport(0, 0, ScreenWidth, ScreenHeight);
+
+	// Set the Window Resizing Callback
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Render Loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Clean Up after the Window is Closed
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
